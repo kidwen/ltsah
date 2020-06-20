8da34e42f71d327cd453e60fb306b5b14111c1ac
kidwen <2061895077@qq.com>
2020-06-20 23:21:24 +0800

    rename myapp to lookme

--- android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.getcapacitor.myapp;
+package com.getcapacitor.lookMe;
 
 import android.content.Context;
 
--- android/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
+++ android/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.getcapacitor.myapp;
+package com.getcapacitor.lookMe;
 
 import org.junit.Test;
 
