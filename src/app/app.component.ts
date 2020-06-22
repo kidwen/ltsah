d4f7bcf09739ef924b5616d498ba20ac4f073af4
kidwen <2061895077@qq.com>
2020-06-22 20:31:24 +0800

    fix lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
     selector: 'app-root',
     template: '<ion-app><ion-router-outlet></ion-router-outlet></ion-app>',
 })
 export class AppComponent {
-    constructor(
+    public constructor(
         private platform: Platform,
         private splashScreen: SplashScreen,
         private statusBar: StatusBar,
@@ -16,7 +16,7 @@ export class AppComponent {
         this.initializeApp();
     }
 
-    initializeApp() {
+    public initializeApp(): void {
         this.platform.ready().then(() => {
             this.statusBar.styleDefault();
             this.splashScreen.hide();
