cb121a0e16dcc2d13d6a46c0375ef31b871bd6be
kidwen <2061895077@qq.com>
2021-01-19 00:15:44 +0800

    add pwa elements

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
 // tslint:disable-next-line: no-unsafe-any
 if (environment.production) {
     enableProdMode();
@@ -11,3 +13,6 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
+
+// Call the element loader after the platform has been bootstrapped
+await defineCustomElements(window);
