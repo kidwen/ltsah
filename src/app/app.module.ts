96fbd2b9aa994026194673058bc96d4309d045b9
kidwen <2061895077@qq.com>
2021-05-10 22:19:07 +0800

    update play music method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { PreloadAllModules, RouteReuseStrategy, RouterModule } from '@angular/router';
 import { MusicControls } from '@ionic-native/music-controls/ngx';
+import { NativeAudio } from '@ionic-native/native-audio/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { TankModule } from './tank/tank.module';
-import { NativeAudio } from '@ionic-native/native-audio/ngx';
 
 @NgModule({
     declarations: [
