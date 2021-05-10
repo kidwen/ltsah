7b08ebcfce17db601692b3dd0b3663043a88fb4d
kidwen <2061895077@qq.com>
2021-05-11 00:25:19 +0800

    add  next and list button

--- src/modules/portal/components/potal-list/potal-list.component.ts
+++ src/modules/portal/components/potal-list/potal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InteractionService } from '@kidwen/shared';
 
 @Component({
     selector: 'app-potal-list',
@@ -10,6 +11,14 @@ export class PotalListComponent {
 
     public audio?: HTMLAudioElement;
 
+    public audioUrls: Array<string> = new Array<string>();
+
+    public currentPosition: number = 0;
+
+    public constructor(
+        private interaction: InteractionService,
+    ) { }
+
     public playMusic(): void {
         if (this.audio) {
             this.audio.currentTime = 0;
@@ -17,14 +26,26 @@ export class PotalListComponent {
             this.audio = new Audio();
             this.audio.src = 'https://aod.cos.tx.xmcdn.com/group72/M04/41/AF/wKgO0F4lfeTAnAnOAKsbpMyVdhs568.m4a';
         }
-        this.audio.play().catch();
+        this.audio.play().catch(error => this.interaction.toast(`播放失败${error}`));
     }
 
     public continueMusic(): void {
-        this.audio?.play().catch();
+        this.audio?.play().catch(error => this.interaction.toast(`播放失败${error}`));
     }
 
     public pauseMusic(): void {
         this.audio?.pause();
+    }
+
+    public nextMusic(): void {
+        if (this.currentPosition >= this.audioUrls.length) {
+            return;
+        }
+        this.audio.src = this.audioUrls[this.currentPosition + 1];
+        this.audio.play().catch(error => this.interaction.toast(`播放失败${error}`));
+    }
+
+    public showAudioList(): void {
+        //  TODO: interaction to show audio list
     }
 }
