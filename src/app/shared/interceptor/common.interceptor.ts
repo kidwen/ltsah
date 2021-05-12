1d4af664b04ac465c5677825d04cc747d0823417
Nierifeng <50561965+Nierifeng@users.noreply.github.com>
2021-05-12 18:28:08 +0800

    add channel component (#27)
    Co-authored-by: Nick <nick@cxist.com>

--- src/app/shared/interceptor/common.interceptor.ts
+++ src/app/shared/interceptor/common.interceptor.ts
@@ -6,7 +6,10 @@ import { Observable } from 'rxjs';
 export class CommonInterceptor implements HttpInterceptor {
     private basisUrl: string = 'http://www.vision123.site';
     public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const url: string = `${this.basisUrl}/${req.url}`;
+        let url: string = req.url;
+        if (url && url.indexOf('http') != 0) {
+            url = `${this.basisUrl}/${req.url}`;
+        }
         let authReq: HttpRequest<any> = req.clone({ url });
         return next.handle(authReq);
     }
---
+++ src/models/channel.model.ts
@@ -0,0 +1,4 @@
+export interface Channel {
+    type_name: string;
+    url: string;
+}
