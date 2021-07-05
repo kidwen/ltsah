792af40ade97bd10510dd7f0460c8f7b3b039300
xuqiujia <xuqiujia@entropyreduce.com>
2021-07-05 17:25:09 +0800

    update @kidwen/tslint version to 1.0.5

--- src/app/shared/interceptor/common.interceptor.ts
+++ src/app/shared/interceptor/common.interceptor.ts
@@ -7,7 +7,7 @@ export class CommonInterceptor implements HttpInterceptor {
     private basisUrl: string = 'https://www.vision123.site';
     public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         let url: string = req.url;
-        if (url && url.indexOf('http') != 0) {
+        if (url && url.indexOf('http') !== 0) {
             url = `${this.basisUrl}/${req.url}`;
         }
         const authReq: HttpRequest<any> = req.clone({ url });
